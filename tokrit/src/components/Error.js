6698d9206df17fb1181e1de63fe462624366ecdf
sungwoong <tjddnd523@gmail.com>
2019-03-14 19:56:48 -0400

    Basic Layout for the landing page using react-router-dom

---
+++ tokrit/src/components/Error.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <p>ERROR: Path does not exist!!</p>
+        </div>    
+    );
+};
+
+export default Error;
