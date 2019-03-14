abaa18fa24d185135c9fd1d692c2419f36795c34
sungwoong <tjddnd523@gmail.com>
2019-03-14 19:44:59 -0400

    Revert "Adding react-router-dom"
    This reverts commit 80351fc19c760cb97dabfc18a3ae2db7b0a8d11a.

--- tokrit/src/App.js
+++ tokrit/src/App.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { Button } from 'reactstrap';
-import Home from './components/Home';
-import About from './components/About';
-import Announcement from './components/Announcement';
-import Menu from './components/Menu';
-import Error from './components/Error';
-import Navigation from './components/Navigation';
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'reactstrap'
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Navigation />
-          <Switch>
-            <Route path = "/" component = {Home} exact/>
-            <Route path = "/announcement" component = {Announcement} />
-            <Route path = "/about" component = {About} />
-            <Route path = "/menu" component = {Menu} />
-            <Route component = {Error} />
-          </Switch>
-        </div>
-      </BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <Button outline color="primary">primary</Button>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
     );
   }
 }
