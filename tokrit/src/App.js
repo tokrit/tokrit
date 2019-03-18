231f7aabe7c5620317aee95b8e840e92caec7bda
Young Jin Kim <youngjin.kim@universe.com>
2019-03-18 19:43:01 -0400

    Changing of file structure

--- tokrit/src/App.js
+++ tokrit/src/App.js
@@ -1,11 +1,5 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-
-import Home from './js/HomePage/Home';
-import About from './js/AboutUs/About';
-import Announcement from './js/Announcement/Announcement';
-import Navigation from './js/NavigationBar/Navigation';
-import LoginPage from './js/LoginPage/LoginPage';
 import { PrivateRoute } from './js/PrivateRoute/PrivateRoute';
 
 import './scss/Home.scss';
@@ -14,13 +8,20 @@ import './scss/Announcement.scss';
 import './scss/Home.scss';
 import './scss/Navigation.scss';
 
+import Home from './routes/HomePage/Home';
+import About from './routes/AboutUs/About';
+import Announcement from './routes/Announcement/Announcement';
+import Login from './routes/LoginPage/LoginPage';
+
+import Navigation from './components/NavigationBar/Navigation';
+
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
           <Navigation isLoggedIn={localStorage.getItem('user')}/>
           <Switch>
-            <Route path="/login" component={LoginPage} />
+            <Route path="/login" component={Login} />
             <Route path="/" component={Home}/>
             <PrivateRoute path = "/announcement" component={Announcement} />
             <PrivateRoute path = "/about" component={About} />
