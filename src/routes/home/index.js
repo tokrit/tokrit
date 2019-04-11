a0d0710c4da034355b35f9ddd0c5f6dfe1eaba1c
Young Jin Kim <youngjin.kim@universe.com>
2019-04-11 00:01:00 -0400

    Layout for main section

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 
+import LogoIcon from '../../assets/home/tokrit-logo.svg';
+import Chevron from '../../assets/home/main-page-chevron.svg';
+
 import styles from './index.module.scss';
 
 class Home extends Component {
   render() {
-    return null;
+    return(
+      <div className={styles.mainPageContainer}>
+        <h1 className={styles.mainHeader}>We Design. We Create. We Collaborate</h1>
+        <div className={styles.logoContainer}>
+          <img src={LogoIcon} className={styles.mainLogoIcon} />
+          <h2 className={styles.findOutLink}>FIND OUT MORE ABOUT WHO WE ARE</h2>
+          <a href=""><img src={Chevron} className={styles.mainChevron} /></a>
+        </div>
+      </div>
+    );
   }
 }
 
