9380b37c6805d8062d144f93d00c773c289038a9
Young Jin Kim <yj42kim@gmail.com>
2019-04-10 22:54:42 -0400

    Navbar Layout (#13)
    * exclude/remove package-lock.json
    * Add assets for navbar
    * main container syles added
    * added navbar styles
    * remove content of homepage

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div>
+        <div className="siteContainer">
           <Navigation isLoggedIn={localStorage.getItem('user')} />
           <Switch>
             <Route path="/login" component={Login} />
