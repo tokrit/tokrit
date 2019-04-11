5198f74d6c5b821c7b898696a2aab3bf6594eab9
Young Jin Kim <youngjin.kim@universe.com>
2019-04-10 22:50:48 -0400

    remove content of homepage

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -4,21 +4,7 @@ import styles from './index.module.scss';
 
 class Home extends Component {
   render() {
-    return (
-      // <div className={styles.mainCover}>
-      //   <div className={styles.mainContainer}>
-      //     <h1>Hackday 1</h1>
-      //     <p>March, 3rd Sunday</p>
-      //     <p>350 Victoria St, Toronto, ON M5B 2K3</p>
-      //     <p>3:00PM ~ 9:PM</p>
-      //   </div>
-      //   <div className={styles.buttonContainer}>
-      //     <button className={styles.button} onClick={this.handleClick}>
-      //       MORE
-      //     </button>
-      //   </div>
-      // </div>
-    );
+    return null;
   }
 }
 
