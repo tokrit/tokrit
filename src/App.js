2a1836aedd254f85da9474ac57ed4cb2e3bca9d6
Sungwoong Pyeon <tjddnd523@gmail.com>
2019-04-26 14:21:24 -0400

    Announcement page (#16)
    * feature/added announcement page with HTML/SCSS files
    * feature/added assets image files for announcement page
    * Minor code refactoring

--- src/App.js
+++ src/App.js
@@ -17,10 +17,10 @@ class App extends Component {
         <div className="siteContainer">
           <Navigation isLoggedIn={localStorage.getItem('user')} />
           <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/announcement" component={Announcement} />
-            <Route exact path="/about" component={About} />
+            <Route exact={true} path="/login" component={Login} />
+            <Route exact={true} path="/" component={Home} />
+            <Route exact={true} path="/announcement" component={Announcement} />
+            <Route exact={true} path="/about" component={About} />
           </Switch>
         </div>
       </BrowserRouter>
