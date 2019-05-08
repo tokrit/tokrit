6fa0033e983e42473919844731b4586af565d88f
Seungjae Lee <sting1283@gmail.com>
2019-05-09 05:53:17 +0900

    Project page
    * CSS refactoring (using nested style)
    * Code formmatting (using with "Prettier")
    * Add to be left out "line" over the page's main picture

--- src/routes/project/index.js
+++ src/routes/project/index.js
@@ -1,75 +1,87 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment } from "react";
 
-import styles from './index.module.scss';
+import styles from "./index.module.scss";
 
 // Images
-import ItLogoIcon from '../../assets/project/tokrit-logo.svg';
+import ItLogoIcon from "../../assets/project/tokrit-logo.svg";
 
 class Project extends Component {
-    render() {
-        return (
-            <Fragment>
-            {/* Header */}
-            <section className={styles.projectPageMain}>
-                <header>
-                    <h1>
-                    Toronto-based<br/>
-                    Young IT Professionals
-                    </h1>
-                    <h2><img src={ItLogoIcon} alt="tokrit"/></h2>
-                </header>
-            </section>
-            {/* Content */}
-            <section className={styles.projectPageContent}>
-                <h3>Project</h3>
-                <section className={styles.projectPageContentSubHeader}>This is what we do. Please take a look and give us any comments! We’d love to hear from you.</section>
-                <section className={styles.projectPageContentServices}>
-                    <a href="#">
-                        <header></header>
-                        <section>Toronto, Canada</section>
-                    </a>
-                    <a href="#">
-                        <header></header>
-                        <section>Toronto, Canada</section>
-                    </a>
-                    <a href="#">
-                        <header></header>
-                        <section>Toronto, Canada</section>
-                    </a>
+  render() {
+    return (
+      <Fragment>
+        {/* Header */}
+        <section className={styles.projectPageMain}>
+          <header>
+            <h1>
+              Toronto-based
+              <br />
+              Young IT Professionals
+            </h1>
+            <h2>
+              <img src={ItLogoIcon} alt="tokrit" />
+              <hr />
+            </h2>
+          </header>
+        </section>
+        {/* Content */}
+        <section className={styles.projectPageContent}>
+          <h3>Project</h3>
+          <section className={styles.projectPageContentSubHeader}>
+            This is what we do. Please take a look and give us any comments!
+            We’d love to hear from you.
+          </section>
+          <section className={styles.projectPageContentServices}>
+            <a href="#">
+              <header />
+              <section>Toronto, Canada</section>
+            </a>
+            <a href="#">
+              <header />
+              <section>Toronto, Canada</section>
+            </a>
+            <a href="#">
+              <header />
+              <section>Toronto, Canada</section>
+            </a>
 
-                    <a href="#">
-                        <header></header>
-                        <section>Toronto, Canada</section>
-                    </a>
-                    <a href="#">
-                        <header></header>
-                        <section>Toronto, Canada</section>
-                    </a>
-                    <a href="#">
-                        <header></header>
-                        <section>Toronto, Canada</section>
-                    </a>
+            <a href="#">
+              <header />
+              <section>Toronto, Canada</section>
+            </a>
+            <a href="#">
+              <header />
+              <section>Toronto, Canada</section>
+            </a>
+            <a href="#">
+              <header />
+              <section>Toronto, Canada</section>
+            </a>
 
-                    <a href="#">
-                        <header></header>
-                        <section>Toronto, Canada</section>
-                    </a>
-                    <a href="#">
-                        <header></header>
-                        <section>Toronto, Canada</section>
-                    </a>
-                    <a href="#">
-                        <header></header>
-                        <section>Toronto, Canada</section>
-                    </a>
-                </section>
+            <a href="#">
+              <header />
+              <section>Toronto, Canada</section>
+            </a>
+            <a href="#">
+              <header />
+              <section>Toronto, Canada</section>
+            </a>
+            <a href="#">
+              <header />
+              <section>Toronto, Canada</section>
+            </a>
+          </section>
 
-                <section className={styles.projectPageContentFooter}>Send us a message if you liked our work. Let’s talk about collaboration!</section>
-                <button className={styles.projectPageContentBtn}>Send us a message</button>
-            </section>
-            </Fragment>
-        );
-    }
+          <section className={styles.projectPageContentFooter}>
+            Send us a message if you liked our work. Let’s talk about
+            collaboration!
+          </section>
+          <button className={styles.projectPageContentBtn}>
+            Send us a message
+          </button>
+        </section>
+      </Fragment>
+    );
+  }
 }
 
 export default Project;
